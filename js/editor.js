c0b30c2ebca07fabf933da14635cb096e42988c6
Farouk858 <faroukalaofa@gmail.com>
2025-09-28 13:08:43 +0100

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -11,7 +11,7 @@ const GH_BRANCH  = 'main';
 
 /* ---------- FEATURE FLAGS (turn on one-by-one once stable) ---------- */
 const ENABLE_BLOCKS_BASIC  = true; // grapesjsBlocksBasic
-const ENABLE_FORMS         = false; // grapesjsPluginForms
+const ENABLE_FORMS         = true; // grapesjsPluginForms
 const ENABLE_NAVBAR        = false; // grapesjsNavbar
 const ENABLE_TABS          = false; // grapesjsTabs
 const ENABLE_CUSTOM_CODE   = false; // grapesjsCustomCode
